f1605f7e0a3abda1e2b9831d1fadf322ffc556bb
Janover Saldaña <u20201b510@upc.edu.pe>
2023-09-13 18:27:03 -0500

    chore:  initial commit

---
+++ src/main/java/com/acme/hellosprintdeveloper/HelloSprintDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.acme.hellosprintdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSprintDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSprintDeveloperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/hellosprintdeveloper/HelloSprintDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.hellosprintdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSprintDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
